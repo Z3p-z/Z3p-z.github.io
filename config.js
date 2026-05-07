b381a6bdd665c31a693ae61aa2f6dd5d16e9ea71
Melo <jannik263@outlook.de>
2026-05-07 16:20:17 +0200

    .

--- config.js
+++
@@ -1,9 +0,0 @@
-// -----------------------------
-// Global Melo Maintenance Config
-// -----------------------------
-
-// Password for /maintenance-admin
-const ADMIN_PASSWORD = "XYZ";
-
-// Load maintenance state from localStorage
-let MAINTENANCE_MODE = localStorage.getItem("maintenance") === "true";
